f5acfa9006df0b012cce6d80f5d91ac9a88c62fa
schillgc <schillgc@gmail.com>
2017-06-25 02:49:11 -0400

    Added XML .Net

--- csharp/Zillow.cs
+++ csharp/Zillow.cs
@@ -5,20 +5,54 @@ namespace JohnBaxterSchillingProperties
       public string zEstimate;
       public string getSearchResults;
       public string zws-id;
-      public int zpid;
-      public bool rentZEstimate;
+      public int zpid = 95196527;
+      public bool rentZEstimate = true;
 
       public string Address : base (addressNumber, streetAddress);
       public string CityStateZip : base (city, state, zipcode);
-      rentZEstimate = true;
+
 
       // Connect to Zillow
       Zillow zillow = new Zillow
       {
           string GetSearchResultsURL = getSearchResults + "?zws-id" + zwid + "&address=" + Address + "&citystatezip=" + CityStateZip + "&rentzestimate=" + rentZEstimate;
 
+          return (GetSearchResultsURL);
+
+
           string ZEstimateURL = zEstimate + "?zws-id=" + zws-id + "&zpid=" + zpid + "&rentzestimate=" + rentZEstimate;
+
+          return (ZEstimateURL);
       }
+
+      public static XmlDocument MakeRequest(string requestUrl)
+      {
+          try
+          {
+              HttpWebRequest request = WebRequest.Create(requestUrl) as HttpWebRequest;
+              HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+
+              XmlDocument xmlDoc = new XmlDocument();
+              xmlDoc.Load(response.GetResponseStream());
+              return (xmlDoc);
+
+          }
+          catch (Exception e)
+          {
+              Console.WriteLine(e.Message);
+
+              Console.Read();
+              return null;
+          }
+      }
+
+      static public void ProcessResponse(XmlDocument locationsResponse)
+      {
+      }
+
+      //Create namespace manager
+      XmlNamespaceManager nsmgr = new XmlNamespaceManager(locationsResponse.NameTable);
+      nsmgr.AddNamespace("rest","http://schemas.microsoft.com/search/local/ws/rest/v1");
 
       var client = new WebClient();
       var content = client.DownloadData(GetSearchResultsURL);
