cb9ddbedf32a38429fc454f7520071e71ac52e62
schillgc <schillgc@gmail.com>
2017-06-26 09:49:16 -0400

    Initial commit

---
+++ csharp/Zillow/key.cs
@@ -0,0 +1,11 @@
+namespace JohnBaxterSchillingProperties
+{
+    class API
+    {
+      // Connecting
+      public Connect (string url)
+      {
+        public int zpid = 95196527;
+      }
+    }
+}
