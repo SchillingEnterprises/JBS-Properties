ad26330b6d21599538e83f60a8ea807c0a2e163d
Gavin Schilling <schillgc@users.noreply.github.com>
2017-07-01 23:25:57 -0400

    Secured

--- csharp/Google/Key.cs
+++
@@ -1,8 +0,0 @@
-namespace Google
-{
-    class Key : GoogleMaps, GoogleAnalyticsApi
-    {
-      public string readonly key = "AIzaSyDdNpQIIQNziuF5szq1DfRNKAUdNY3yNp4" : base (key);
-      public string readonly tid = "UA-84234707-4" : base (tid);
-    }
-}
