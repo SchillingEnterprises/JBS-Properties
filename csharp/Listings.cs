921c59ea430d23b1ec577cf756d09063cd50a9b4
schillgc <schillgc@gmail.com>
2017-07-17 04:06:31 -0400

    Added Latitude & Longitude

--- csharp/Listings.cs
+++ csharp/Listings.cs
@@ -2,16 +2,20 @@ using System;
 
 namespace JBSProperties
 {
-    class Listing : Zillow
+  class public static void Main(){
+    class Listing : Zillow, GoogleMaps
     {
 
       public string Address : base (Address);
       public string CityStateZip : base (CityStateZip);
+      public double Latitude : base (Latitude);
+      public double Longitude : base (Longitude);
 
       Listing listing = new Listing (int addressNumber, string streetAddress, string city, string state, int zipCode)
       {
-        Console.WriteLine(addressNumber + " " + streetAddress);
-        Console.WriteLine(city + " " + state + ", " + zipCode);
+        Console.WriteLine (addressNumber + " " + streetAddress);
+        Console.WriteLine (city + " " + state + ", " + zipCode);
       }
     }
+  }
 }
