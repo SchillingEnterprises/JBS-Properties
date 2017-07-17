38f067400dd918d189aee6daba3586e2f804a982
schillgc <schillgc@gmail.com>
2017-07-17 04:09:06 -0400

    Cosmetic

--- csharp/Google/GoogleAnalyticsApi.cs
+++ csharp/Google/GoogleAnalyticsApi.cs
@@ -16,27 +16,27 @@ namespace Generic.Core.WebContext
 {
     public class GoogleAnalyticsApi
     {
-        public static void TrackEvent(string category, string action, string label, int? value = null)
+        public static void TrackEvent (string category, string action, string label, int? value = null)
         {
-            Track(HitType.@event, category, action, label, value);
+            Track (HitType.@event, category, action, label, value);
         }
 
-        public static void TrackPageview(string category, string action, string label, int? value = null)
+        public static void TrackPageview (string category, string action, string label, int? value = null)
         {
-            Track(HitType.@pageview, category, action, label, value);
+            Track (HitType.@pageview, category, action, label, value);
         }
 
-        private static void Track(HitType type, string category, string action, string label,
+        private static void Track (HitType type, string category, string action, string label,
                                   int? value = null)
         {
-            if (string.IsNullOrEmpty(category)) throw new ArgumentNullException("category");
-            if (string.IsNullOrEmpty(action)) throw new ArgumentNullException("action");
+            if (string.IsNullOrEmpty (category)) throw new ArgumentNullException ("category");
+            if (string.IsNullOrEmpty(action)) throw new ArgumentNullException ("action");
 
-            var request = (HttpWebRequest) WebRequest.Create("http://www.google-analytics.com/collect");
+            var request = (HttpWebRequest) WebRequest.Create ("http://www.google-analytics.com/collect");
             request.Method = "POST";
 
             // the request body we want to send
-            var postData = new Dictionary<string, string>
+            var postData = new Dictionary <string, string>
                            {
                                { "v", "1" },
                                { "tid", Google.Key.tid },
@@ -45,35 +45,35 @@ namespace Generic.Core.WebContext
                                { "ec", category },
                                { "ea", action },
                            };
-            if (!string.IsNullOrEmpty(label))
+            if (!string.IsNullOrEmpty (label))
             {
-                postData.Add("el", label);
+                postData.Add ("el", label);
             }
             if (value.HasValue)
             {
-                postData.Add("ev", value.ToString());
+                postData.Add ("ev", value.ToString());
             }
 
             var postDataString = postData
-                .Aggregate("", (data, next) => string.Format("{0}&{1}={2}", data, next.Key,
-                                                             HttpUtility.UrlEncode(next.Value)))
+                .Aggregate ("", (data, next) => string.Format ("{0}&{1}={2}", data, next.Key,
+                                                             HttpUtility.UrlEncode (next.Value)))
                 .TrimEnd('&');
 
             // set the Content-Length header to the correct value
-            request.ContentLength = Encoding.UTF8.GetByteCount(postDataString);
+            request.ContentLength = Encoding.UTF8.GetByteCount (postDataString);
 
             // write the request body to the request
-            using (var writer = new StreamWriter(request.GetRequestStream()))
+            using (var writer = new StreamWriter (request.GetRequestStream () ))
             {
-                writer.Write(postDataString);
+                writer.Write (postDataString);
             }
 
             try
             {
-                var webResponse = (HttpWebResponse) request.GetResponse();
+                var webResponse = (HttpWebResponse) request.GetResponse ();
                 if (webResponse.StatusCode != HttpStatusCode.OK)
                 {
-                    throw new HttpException((int) webResponse.StatusCode,
+                    throw new HttpException ((int) webResponse.StatusCode,
                                             "Google Analytics tracking did not return OK 200");
                 }
             }
